2939bcdb962b1f71ae820c93f26ce26361bad84f
Lucas <somfic@gmail.com>
2022-08-05 11:29:48 +0200

    Created tracker

---
+++ tracker/src/in_game/in_game.ts
@@ -0,0 +1,176 @@
+import { OWGames, OWGamesEvents, OWHotkeys } from "@overwolf/overwolf-api-ts";
+
+import { AppWindow } from "../AppWindow";
+import { kHotkeys, kWindowNames, kGamesFeatures } from "../consts";
+
+import WindowState = overwolf.windows.WindowStateEx;
+
+// The window displayed in-game while a game is running.
+// It listens to all info events and to the game events listed in the consts.ts file
+// and writes them to the relevant log using <pre> tags.
+// The window also sets up Ctrl+F as the minimize/restore hotkey.
+// Like the background window, it also implements the Singleton design pattern.
+class InGame extends AppWindow {
+  private static _socket: WebSocket;
+
+  private static _instance: InGame;
+  private _gameEventsListener: OWGamesEvents;
+  private _eventsLog: HTMLElement;
+  private _infoLog: HTMLElement;
+
+  private constructor() {
+    super(kWindowNames.inGame);
+
+    InGame.connect();
+
+    this._eventsLog = document.getElementById("eventsLog");
+    this._infoLog = document.getElementById("infoLog");
+
+    this.setToggleHotkeyBehavior();
+    this.setToggleHotkeyText();
+  }
+
+  public static instance() {
+    if (!this._instance) {
+      this._instance = new InGame();
+    }
+
+    return this._instance;
+  }
+
+  private static connect() {
+    InGame._socket = new WebSocket("ws://localhost:3000/ws");
+    InGame._socket.onopen = function () {
+      InGame._socket.send(
+        JSON.stringify({
+          identity: "tracker",
+        })
+      );
+    };
+
+    InGame._socket.onmessage = function (e) {
+      console.log("Message:", e.data);
+    };
+
+    InGame._socket.onclose = function (e) {
+      console.log(
+        "Socket is closed. Reconnect will be attempted in 1 second.",
+        e.reason
+      );
+
+      setTimeout(() => InGame.connect(), 1000);
+    };
+
+    InGame._socket.onerror = function (err) {
+      console.error("Socket encountered error: ", err, "Closing socket");
+      InGame._socket.close();
+    };
+  }
+
+  public async run() {
+    const gameClassId = await this.getCurrentGameClassId();
+
+    const gameFeatures = kGamesFeatures.get(gameClassId);
+
+    if (gameFeatures && gameFeatures.length) {
+      this._gameEventsListener = new OWGamesEvents(
+        {
+          onInfoUpdates: this.onInfoUpdates.bind(this),
+          onNewEvents: this.onNewEvents.bind(this),
+        },
+        gameFeatures
+      );
+
+      this._gameEventsListener.start();
+    }
+  }
+
+  private onInfoUpdates(info) {
+    this.logLine(this._infoLog, info, false);
+  }
+
+  // Special events will be highlighted in the event log
+  private onNewEvents(e) {
+    const shouldHighlight = e.events.some((event) => {
+      switch (event.name) {
+        case "kill":
+        case "death":
+        case "assist":
+        case "level":
+        case "matchStart":
+        case "match_start":
+        case "matchEnd":
+        case "match_end":
+          return true;
+      }
+
+      return false;
+    });
+    this.logLine(this._eventsLog, e, shouldHighlight);
+  }
+
+  // Displays the toggle minimize/restore hotkey in the window header
+  private async setToggleHotkeyText() {
+    const gameClassId = await this.getCurrentGameClassId();
+    const hotkeyText = await OWHotkeys.getHotkeyText(
+      kHotkeys.toggle,
+      gameClassId
+    );
+    const hotkeyElem = document.getElementById("hotkey");
+    hotkeyElem.textContent = hotkeyText;
+  }
+
+  // Sets toggleInGameWindow as the behavior for the Ctrl+F hotkey
+  private async setToggleHotkeyBehavior() {
+    const toggleInGameWindow = async (
+      hotkeyResult: overwolf.settings.hotkeys.OnPressedEvent
+    ): Promise<void> => {
+      console.log(`pressed hotkey for ${hotkeyResult.name}`);
+      const inGameState = await this.getWindowState();
+
+      if (
+        inGameState.window_state === WindowState.NORMAL ||
+        inGameState.window_state === WindowState.MAXIMIZED
+      ) {
+        this.currWindow.minimize();
+      } else if (
+        inGameState.window_state === WindowState.MINIMIZED ||
+        inGameState.window_state === WindowState.CLOSED
+      ) {
+        this.currWindow.restore();
+      }
+    };
+
+    OWHotkeys.onHotkeyDown(kHotkeys.toggle, toggleInGameWindow);
+  }
+
+  // Appends a new line to the specified log
+  private logLine(log: HTMLElement, data, highlight) {
+    const line = document.createElement("pre");
+    line.textContent = JSON.stringify(data);
+
+    InGame._socket.send(JSON.stringify(data));
+
+    if (highlight) {
+      line.className = "highlight";
+    }
+
+    // Check if scroll is near bottom
+    const shouldAutoScroll =
+      log.scrollTop + log.offsetHeight >= log.scrollHeight - 10;
+
+    log.appendChild(line);
+
+    if (shouldAutoScroll) {
+      log.scrollTop = log.scrollHeight;
+    }
+  }
+
+  private async getCurrentGameClassId(): Promise<number | null> {
+    const info = await OWGames.getRunningGameInfo();
+
+    return info && info.isRunning && info.classId ? info.classId : null;
+  }
+}
+
+InGame.instance().run();
